d958139c257eccf44c7657b22664305b5161d3bc
samirasamadi <s.samadi@gmail.com>
2017-06-30 23:22:18 -0700

    Update practice.js

--- js/practice.js
+++ js/practice.js
@@ -8,6 +8,7 @@ var vowels = "aeiou";
 var siteNames = ['google' ,'facebook' ,'youtube' ,'baidu' ,'yahoo' ,'amazon' ,'wikipedia' ,'qq' ,'twitter' ,'google' ,'taobao' ,'live' ,'sina' ,'linkedin' ,'yahoo' ,'weibo' ,'ebay' ,'google' ,'yandex' ,'hao123' ,'bing' ,'google' ,'msn' ,'instagram' ,'amazon' ,'aliexpress' ,'google' ,'apple' ,'blogspot' ,'pinterest' ,'ask' ,'wordpress' ,'tmall' ,'google' ,'reddit' ,'google' ,'mail' ,'paypal' ,'onclickads' ,'tumblr' ,'sohu' ,'google' ,'imgur' ,'microsoft' ,'google' ,'imdb' ,'gmw' ,'netflix' ,'fc2' ,'amazon' ,'360' ,'googleadservices' ,'alibaba' ,'stackoverflow' ,'google' ,'google' ,'craigslist' ,'google' ,'tianya' ,'rakuten' ,'amazon' ,'blogger' ,'naver' ,'espn' ,'google' ,'soso' ,'cnn' ,'outbrain' ,'nicovideo' ,'google' ,'dropbox' ,'bbc' ,'flipkart' ,'github' ,'diply' ,'googleusercontent' ,'adcash' ,'xinhuanet' ,'amazon' ,'google' ,'google' ,'ebay' ,'google' ,'popads' ,'dailymotion' ,'pixnet' ,'nytimes' ,'ebay' ,'sogou' ,'booking' ,'bbc' ,'jdsports' ,'163' ,'dailymail' ,'livedoor' ,'adobe' ,'wikia' ,'adnetworkperformance' ,'chase' ,'china' ,'indiatimes' ,'uol' ,'china' ,'coccoc' ,'huffingtonpost' ,'google' ,'alipay' ,'directrev' ,'buzzfeed' ,'dmm' ,'youku' ,'google' ,'amazon' ,'cntv' ,'blogspot' ,'google' ,'google' ,'ameblo' ,'google' ,'amazon' ,'microsoftonline' ,'theguardian' ,'amazonaws' ,'bankofamerica' ,'google' ,'chinadaily' ,'walmart' ,'slideshare' ,'cnet' ,'etsy' ,'daum' ,'yelp' ,'ettoday' ,'globo' ,'twitch' ,'tudou' ,'stackexchange' ,'aol' ,'quora' ,'whatsapp' ,'indeed' ,'flickr' ,'weather' ,'google' ,'tradeadexchange' ,'office' ,'amazon' ,'naver' ,'soundcloud' ,'snapdeal' ,'bp' ,'forbes' ,'wellsfargo' ,'douban' ,'zillow' ,'office365' ,'google' ,'vice' ,'gmail' ,'leboncoin' ,'google' ,'godaddy' ,'ikea' ,'salesforce' ,'vimeo' ,'google' ,'kakaku' ,'detail' ,'target' ,'goo' ,'about' ,'tripadvisor' ,'livejournal' ,'avito' ,'bestbuy' ,'allegro' ,'foxnews' ,'wordpress' ,'feedly' ,'theladbible' ,'themeforest' ,'deviantart' ,'9gag' ,'w3schools' ,'nfl' ,'washingtonpost' ,'nih' ,'wikihow' ,'files' ,'businessinsider' ,'google' ,'skype' ,'gfycat' ,'taboola' ,'telegraph' ,'softonic' ,'google' ,'avg' ,'mozilla' ,'nametests' ,'mediafire' ,'onet' ,'americanexpress' ,'pixiv' ,'mystart' ,'zol' ,'ups' ,'google' ,'people' ,'wix' ,'amazon' ,'homedepot' ,'archive' ,'doorblog' ,'huanqiu' ,'weebly' ,'google' ,'secureserver' ,'usps' ,'google' ,'comcast' ,'akamaihd' ,'force' ,'steamcommunity' ,'wikimedia' ,'bitauto' ,'zhihu' ,'addthis' ,'ndtv' ,'google' ,'google' ,'ebay-kleinanzeigen' ,'mercadolivre' ,'shutterstock' ,'pcgamer' ,'loading-delivery2' ,'terrapops' ,'xywy' ,'detik' ,'ifeng' ,'web' ,'bilibili' ,'tlbb8' ,'google' ,'popcash' ,'github' ,'bild' ,'usatoday' ,'mailchimp' ,'steampowered' ,'sourceforge' ,'orange' ,'answers' ,'hdfcbank' ,'uptodown' ,'fbcdn' ,'gmx' ,'hp' ,'xfinity' ,'speedtest' ,'dmm' ,'varzesh3' ,'google' ,'webmd' ,'wordreference' ,'pconline' ,'google' ,'att' ,'hootsuite' ,'rambler' ,'groupon' ,'stumbleupon' ,'goodreads' ,'life' ,'spiegel' ,'bloomberg' ,'washingtonpost' ,'capitalone' ,'icicibank' ,'blog' ,'youm7' ,'google' ,'caijing' ,'t-online' ,'fedex' ,'google' ,'engadget' ,'haiwainet' ,'amazon' ,'mashable' ,'thesaurus' ,'adidas' ,'spotify' ,'blogfa' ,'nownews' ,'2channel' ,'samsung' ,'wsj' ,'accuweather' ,'watsons' ,'pandora' ,'ign' ,'udn' ,'gsmarena' ,'mama' ,'google' ,'bet365' ,'seznam' ,'paytm' ,'chaoshi' ,'badoo' ,'media' ,'verizonwireless' ,'styletv' ,'mlb' ,'hulu' ,'youth' ,'ebay' ,'abs-cbnnews' ,'google' ,'reuters' ,'dell' ,'livedoor' ,'zendesk' ,'google' ,'rediff' ,'bleacherreport' ,'1688' ,'ijreview' ,'trello' ,'putlocker' ,'twimg' ,'likes' ,'oeeee' ,'onlinesbi' ,'tistory' ,'reference' ,'jabong' ,'rt' ,'google' ,'sahibinden' ,'smzdm' ,'google' ,'quikr' ,'iqiyi' ,'icloud' ,'milliyet' ,'impress' ,'mega' ,'yaolan' ,'ask' ,'macys' ,'google' ,'evernote' ,'infusionsoft' ,'enet' ,'google' ,'haosou' ,'theverge' ,'baike' ,'kickstarter' ,'cbssports' ,'slickdeals' ,'google' ,'kouclo' ,'webce' ,'lady8844' ,'thefreedictionary' ,'kohls' ,'eksisozluk' ,'ebay' ,'liveinternet' ,'oracle' ,'google' ,'babytree' ,'gameforge' ,'google' ,'ebay' ,'hurriyet' ,'4shared' ,'ppomppu' ,'elpais' ,'meetup' ,'repubblica' ,'chinatimes' ,'blogimg' ,'ewt' ,'taleo' ,'teepr' ,'goal' ,'scribd' ,'libero' ,'newegg' ,'photobucket' ,'gizmodo' ,'eastday' ,'lifehacker' ,'marca' ,'olx' ,'yandex' ,'sberbank' ,'neobux' ,'buzzfil' ,'qunar' ,'google' ,'ltn' ,'livedoor' ,'slack' ,'citi' ,'uploaded' ,'ameba' ,'stockstar' ,'vid' ,'time' ,'fiverr' ,'naukri' ,'cloudfront' ,'espncricinfo' ,'lowes' ,'kompas' ,'gap' ,'aparat' ,'cnzz' ,'xe' ,'disqus' ,'techcrunch' ,'independent' ,'xda-developers' ,'retailmenot' ,'pinimg' ,'savefrom' ,'liputan6' ,'hotels' ,'hm' ,'battle' ,'justdial' ,'nbcnews' ,'bhaskar' ,'nordstrom' ,'shopclues' ,'mobile' ,'gmarket' ,'timeanddate' ,'surveymonkey' ,'gamefaqs' ,'autohome' ,'lenovo' ,'corriere' ,'wunderground' ,'rbc' ,'expedia' ,'albawabhnews' ,'tabelog' ,'google'];
 var lmap = new Array(22);
 letters2 = ['b','c','d','f','g','h','j', 'k','l','m','n','p','q','r','s','t', 'v', 'w','x', 'y','z'];
+var words = new Array(40);
 
 function gid(id) {
          return document.getElementById(id);
@@ -202,7 +203,6 @@ function respond() {
 
 }
 
-
 function I_eval_1(){
             
             var guess1 = gname("I100").value.trim();
@@ -285,12 +285,11 @@ function is_map_full(){
             for (i = 1; i < 21; i++) {
                 if(lmap[i]==null){
                     t=0;
-                }
-                
+                }  
             }
             
             if(t==1){
-                set_mapping();
+                set_lmapping();
                 gname("map_not_full").style.display='none';
                 //saveLMapping();
             }else{
@@ -298,9 +297,33 @@ function is_map_full(){
             }
         }
 
-function set_mapping(){
+function set_lmapping(){
             for(i=1;i<21;i++){
                 gname("lmap"+i).value=lmap[i];
+                gname("p_lmap"+i).value = lmap[i];
+             }
+}
+
+function set_wmapping(){
+         for(i=1;i<21;i++){
+                  gname("p_word"+i).value = wmap[i];
+         }
+}
+
+function is_words_full(){   
+            t=1;
+            for ( i=1;i<21;i++){
+                if(gname("word"+i).value.trim()==""){
+                    t=0;
+                }else{
+                    words[i]=gname("word"+i).value.trim();
+                }
+            }
+            if(t==1){
+                gname("words_not_full").style.display='none';
+                set_wmapping();
+            }else{
+                gname("words_not_full").style.display='block';
             }
         }
 
